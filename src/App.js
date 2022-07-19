a7c41808b60a759edea025f9fa55171613f9a91b
GauravAdz <gaurav.adhikari222@gmail.com>
2022-07-19 17:18:05 +0545

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  <div className="Main_Page">
+  <div className="nav">
+    <h1 className="logo" id="Logo" onclick="{ChangeText}">Protech</h1>
+    <ul className="nav-links">
+      <li><a href="#">HOME</a></li>
+      <li><a href="#">VIDEOS</a></li>
+      <li><a href="#">CONTACTS</a></li>
+      <li><a href="#">ABOUT HER</a></li>
+    </ul>
+  </div>
+  <div className="content">
+    <h1>BECOME A PROTECH-TOR</h1>
+    <p>Subscribe to Pro-tech Gaming <br/> and Witness Top-Class Content From the Legend Himself !</p>
+    <div>
+      <button><span></span><a href="https://www.youtube.com/watch?v=OEmZR0nxuUY
+        https://www.youtube.com/watch?v=OEmZR0nxuUY"  style={{textDecoration: 'none', color: 'white',}}>TRENDING  CONTENT</a></button>
+      <button className= "youtube" type="button"><span className="yt"></span><a href="https://www.youtube.com/channel/UCVo0mSD2bHvHoe73yuoWPwQ/subscribe" style={{textDecoration: 'none', color: 'white',}}>SUBSCRIBE</a></button>
+
     </div>
+  </div>
+</div>
   );
 }
 
